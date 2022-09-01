029024ed35c2e892d8effc2b04de4a938342403b
GatmanVitalie <95142233+GatmanVitalie@users.noreply.github.com>
2022-09-01 22:52:40 +0300

    Add files via upload

---
+++ console.log('Hello World');.java
@@ -0,0 +1,95 @@
+console.log('Hello World');
+
+
+/*Republica Moldova a obținut recunoașterea oficială a statalității, la 2 martie 1992, când a obținut statutul de membru al ONU.
+
+Declarația de Independență a Republicii Moldova a fost un document adoptat de către Parlamentul Republicii Moldova.
+
+Documentul face referiri la „istoria milenară” și „statalitatea neîntreruptă” a românilor de la est de Prut în spațiul istoric și etnic al etnogenezei acestora, consacră, pentru spațiul de la est de Prut, denumire oficială: limba română. Acest act fondator al Republicii Moldova din 27 august 1991 este marcat în fiece an ca Ziua Națională a Republicii Moldova sau Ziua Independenței.
+
+Declarația a fost adoptată și semnată de 278 deputați în parlament la 27 august 1991. Actul original a ars în timpul protestelor din 2009 la Chișinău, dar un document identic a fost restaurat în 2010.
+
+Declarația de independență a Republicii Moldova în mod clar și direct, susține suveranitatea Republicii Moldova pe teritoriul Transnistriei, deoarece este "o parte componentă a teritoriului istoric și etnic al poporului nostru". Cu toate acestea, Declarația de independență a Republicii Moldova este ea însăși un argument împotriva suveranității Republicii Moldova asupra Transnistriei, cum îl denunță acordul de la 23 august 1939, între Guvernul URSS și 
+Guvernul Germaniei, „nulă și neavenită” fiind uniunea doar formală între cele două teritorii.
+Antichitate
+Articol principal: Dacia.
+
+Teritoriul actual al R. Moldova a fost populat în antichitate de triburile dacice ale costobocilor și tyrageților (Hartă a Europei de sud-est în secolul I î.Hr, întocmită de istoricul german Johann Gustav Droysen în 1886.).
+În antichitate pe teritoriul Republicii Moldova, României (partea central nord-estică) și Ucrainei (partea sud-vestică) s-a format civilizația Cucuteni, una dintre cele mai vechi civilizații din Europa. Civilizația a dispărut misterios după migrările popoarelor indo-europene prin spațiul carpato-danubiano-pontic. Astfel s-a format civilizația dacilor.
+
+Strabon în „Geografia” menționa că geții aveau aceeași limbă cu tracii, iar dacii aceeași limbă cu geții.[19] Totuși, prima relatare despre geți aparține lui Herodot.[20][21] Cucerirea Daciei de către romani conduce la contopirea celor două culturi: daco-romanii sunt strămoșii poporului român.[22] După ce Dacia a devenit provincie a Imperiului Roman s-au impus elemente de cultură și civilizație romană, inclusiv latina vulgară care a stat la baza formării limbii române.[23][24][25]
+
+Pe baza informațiilor din inscripția de la Dionysopolis[26][27][28] și de la Iordanes, se știe că sub stăpânirea lui Burebista, ajutat de marele preot Deceneu, s-a format primul stat geto-dac.[29][30] În anul 44 î.Hr., Burebista este asasinat de unul dintre slujitorii săi.[31] După moartea lui, statul geto-dac se va destrăma în 4, apoi în 5 regate.[32] Nucleul statal se menține în zona munților Șureanu, unde domnesc succesiv Deceneu, Comosicus și Coryllus.[33] Statul centralizat dac va atinge apogeul dezvoltării sale sub Decebal.[34] În această perioadă se mențin o serie de conflic
+te cu Imperiul Roman, o partea a statului dac 
+fiind cucerită în 106 d.Hr. de împăratul roman Traian.[35] Între anii 271
+-275 d.Hr. are loc retragerea aureliană.[36]
+
+
+Împăratul Constantin cel Mare a construit în 328, podul lui Constantin cel Mare peste Dunăre, în vederea recuceririi Daciei. În 332 pornește o campanie împotriva vizigoților, îi înfrânge și, ca urmare, ținuturile de la nord de Dunăre inclusiv sudul Moldovei reintră pentru o perioadă sub stăpânire romană. Limita de nord din Dacia a imperiului este marcată de Brazda lui Novac[37]. Limesul continua apoi spre sudul Moldovei (probabil pe râul Buzău) mergând pe la nord de actualul oraș Galați apoi spre Prut pe la Vadul lui Isac și se termina la lacul (limanul) Sasîc, aproape de Nistru, pe valul de pământ numit uneori și valul lui Athanaric [38] Cu această ocazie, Constantin și-a adăugat și titlul de Dacicus Maximus.
+
+Situată pe o rută strategică între Asia și Europa, ținuturile Moldovei au fost deseori prădate sau invadate temporar pe parcursul istoriei antice și medievale de diverse populații sau popoare migratoare, printre care se pot aminti: sarmați, goți (germanici), huni (mongolici), gepizi, avari, bulgarii turcofoni (inițial, apoi slavofoni), varegi, maghiari (ugro-finici), pecenegi și cumani (turcofoni), și tătari.[39]
+
+
+După marea invazie mongolă (1241), întreaga regiune este controlată politic de către Hoarda de Aur.[40] Principatul Moldovei, întemeiat la jumătatea secolului al XIV-lea, include în hotarele sale către sfârșitul secolului, teritoriul dintre Carpați, Nistru, Dunăre și Marea Neagră. Odată ce popoarele migratoare au trecut, s-au asimilat sau așezat (în Europa sau/și Asia), populația băștinasă, vorbitoare de limbă română, sub conducerea lui Bogdan I, Alexandru cel Bun, Ștefan cel Mare și alți domnitori reușește să fortifice frontiera răsăriteană (de est) a Moldovei cu mai multe cetăți românești: Hotin, Soroca, Orhei, Tighina, Cetatea Albă, Chilia[39] După ocuparea cetăților Cetatea Albă și Chilia în 1484, Imperiul Otoman transformă în 1538, la sfârșitul primei domnii a lui Petru Rareș, Tighina și Basarabia în raia turcească (în Evul Mediu Dezvoltat, după apariția țărilor române medievale, Basarabia era denumirea românească a regiunii cunoscută de Turci ca Bugeac).[39]
+
+În secolul al XVI-lea, Principatul Moldovei a fost obligat să plătească tribut la Poarta Otomană fără însă să-și piardă suzeranitatea. Devenit în vremea domniei Ecaterinei a II-a, vecin răsăritean al Moldovei, Imperiul Rus anexează (în mod fraudulos față de tratatele de drept politic aflate atunci în vigoare[41]) prin Pacea de la București din 1812, teritoriul cuprins între Prut și Nistru, parte componentă a Țării Moldovei (45.600 km2, cu o populație de cca 500.000 de locuitori, în proporție de 86% români), denumindu-l Gubernia Basarabia.[39]
+
+Perioada țaristă
+
+
+
+
+
+
+
+După anexarea Basarabiei de către Imperiul Rus în 1812, în Basarabia de Sud începând de la Căușeni și până la Marea Neagră (pe teritoriile locuite anterior de tătarii nogai și turci) are loc colonizarea cu populație creștină – moldoveni, găgăuzi, bulgari, ucraineni, ruși, polonezi, germani, evrei. În toată Basarabia moldovenii au fost expuși unui proces de rusificare și deznaționalizare, astfel în 1867 limba română este interzisă în școlile din Basarabia.[39][42]
+
+Dezvoltarea economică a guberniei Basarabia (după dezlipirea de Moldova), sub adminstrația rusească, s-a efectuat în perspectiva unei economii de export de grâne și vite spre portul Odesa și în paralel cu un proces de rusificare intensă a acestei regiuni, cum se proceda în toate zonele de populație ne-rusă din imperiul țarist rus. În acest cadru au fost construite căile ferate și zonele urbane noi din Chișinău și Bălți, care au devenit (demografic) orașe preponderent rusești în mijlocul autohtonilor români-moldoveni.
+
+Pentru a îndepărta Rusia de la gurile Dunării, marile puteri europene, prin Tratatul de la Paris din 1856, care încheie războiul Crimeii, i-au restituit Principatului Moldovei trei județe din sudul Basarabiei: Cahul, Bolgrad și Ismail. La 24 ianuarie 1859, Principatul Moldovei, prin unirea cu cel al Țării Românești sub domnia unică a românului moldovean Alexandru Ioan Cuza, a participat la întemeierea statului național modern român (care cuprindea atunci și sudul Basarabiei).[39]
+
+Prin Tratatul de la Berlin (1878), marile puteri au obligat România să cedeze înapoi Imperiului Rus cele trei județe din sudul Basarabiei (Cahul, Bolgrad și Ismail) în schimbul Dobrogei și a recunoașterii, de jure, a independenței României față de Imperiul Otoman.[39]
+
+Prima independență
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
