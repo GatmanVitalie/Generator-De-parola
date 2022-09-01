d726dea9055c21140dba7ee6e828d5bd2b6be552
GatmanVitalie <95142233+GatmanVitalie@users.noreply.github.com>
2022-09-01 22:19:05 +0300

    Add files via upload

---
+++ RayCastManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastManager : MonoBehaviour
+{
+    [Header("IsGrounded")]
+    public bool isGrounded;
+
+    [Header("IsSloupt")]
+    public bool isSloupt;
+
+    [Header("Clrouch")]
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
